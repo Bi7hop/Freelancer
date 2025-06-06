7500f742c160ef5113751879b5c62277b658fcd6
Marcel Menke <marcel.menke1981@gmail.com>
2025-06-06 10:23:13 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,14 +14,18 @@ export const routes: Routes = [
     path: 'projects',
     loadComponent: () => import('./features/projects/projects.component').then(m => m.ProjectsComponent)
   },
-//   {
-//     path: 'clients',
-//     loadComponent: () => import('./features/clients/clients.component').then(m => m.ClientsComponent)
-//   },
-//   {
-//     path: 'invoices',
-//     loadComponent: () => import('./features/invoices/invoices.component').then(m => m.InvoicesComponent)
-//   },
+  {
+    path: 'clients',
+    loadComponent: () => import('./features/clients/clients.component').then(m => m.ClientsComponent)
+  },
+  {
+    path: 'invoices',
+    loadComponent: () => import('./features/invoices/invoices.component').then(m => m.InvoicesComponent)
+  },
+  {
+    path: 'settings',
+    loadComponent: () => import('./features/settings/settings.component').then(m => m.SettingsComponent)
+  },
   {
     path: '**',
     redirectTo: '/dashboard' // 404 Fallback
