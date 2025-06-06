1bcebfbefa4d9e7b3cc5789a8a1f8808bec95c86
Marcel Menke <marcel.menke1981@gmail.com>
2025-06-06 08:44:01 +0200

    Added routes to app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent)
+  },
+  {
+    path: 'projects',
+    loadComponent: () => import('./features/projects/projects.component').then(m => m.ProjectsComponent)
+  },
+//   {
+//     path: 'clients',
+//     loadComponent: () => import('./features/clients/clients.component').then(m => m.ClientsComponent)
+//   },
+//   {
+//     path: 'invoices',
+//     loadComponent: () => import('./features/invoices/invoices.component').then(m => m.InvoicesComponent)
+//   },
+  {
+    path: '**',
+    redirectTo: '/dashboard' // 404 Fallback
+  }
+];
